357c29c05ab499a66e91b450632a7c8f9d0a0c71
ChrisFiola <36894544+ChrisFiola@users.noreply.github.com>
2022-04-07 19:03:32 +0000

    Deploying to gh-pages from @ ChrisFiola/chrisfiola.github.io@b1185556dc7c235821344d074139295d2fa56002 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,7 +2,7 @@ var store = [{
         "title": "2021/2022",
         "excerpt":"This is a test for the hall of fame of 2021/2022 season ","categories": [],
         "tags": [],
-        "url": "/hof/test/"
+        "url": "/hof/hof2122/"
       },{
         "title": "Welcome to my first post and to my new website",
         "excerpt":"So I’ve decided to create a website for myself. Hopefully this will help promote myself and eventually gather more interest into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general information about...","categories": [],
