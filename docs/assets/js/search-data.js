209ecdfdc61202472ce4e6a89c512f5b97bde20f
Christopher Fiola <christopherfiola@gmail.com>
2022-04-07 10:57:57 -0400

    Build site

--- docs/assets/js/search-data.js
+++ docs/assets/js/search-data.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Welcome to my first post and to my new website",
         "excerpt":"So I’ve decided to create a website for myself. Hopefully this will help promote myself and eventually gather more interest into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general information about...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/updates/new-update/"
+        "url": "/updates/new-update/"
       }]
