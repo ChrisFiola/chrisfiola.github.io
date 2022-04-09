e9f5fe6feeb68fd9b1901b53bafbc8fe54c93c98
ChrisFiola <36894544+ChrisFiola@users.noreply.github.com>
2022-04-09 21:14:44 +0000

    Deploying to gh-pages from @ ChrisFiola/chrisfiola.github.io@888609de0395ca28541361a9635bd6bc8775a96a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "/hof/hof2122/"
       },{
         "title": "Welcome to my first post and to my new website",
-        "excerpt":"So I’ve decided to create a website for myself. Hopefully this will help promote myself and eventually gather more interest into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general information about...","categories": [],
+        "excerpt":"So I have decided to create a website about myself. Hopefully this will help promote me and eventually gather more attention into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general information...","categories": [],
         "tags": [],
         "url": "/updates/new-update/"
       },]
