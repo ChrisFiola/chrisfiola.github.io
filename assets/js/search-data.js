23a070d3fca41d8c0b767abb5d59e39eee4d7199
ChrisFiola <36894544+ChrisFiola@users.noreply.github.com>
2022-04-29 12:23:42 +0000

    Deploying to gh-pages from @ ChrisFiola/chrisfiola.github.io@46b44978401f864ca5da25f359b94c5deaf9f687 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "2021/2022",
         "excerpt":"You will find the best results of the season on this page. Best 500m of the season 🥇 Best 1000m of the season 🥇 Best 1500m of the season 🥇 Season Bests Season Bests Christopher Fiola (CAN) 500 34,44 Calgary (CAN) 2022-03-19 1000 1.08,43 Calgary (CAN) 2022-03-19 1500 1.46,86 Calgary...","categories": [],
         "tags": [],
-        "url": "/hof/hof2122/"
+        "url": "https://www.chrisfiola.ca/home/hof/hof2122/"
       },{
         "title": "Welcome to my first post and to my new website",
         "excerpt":"So I have decided to create a website about myself. Hopefully this will help promote me and eventually gather more attention into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general information...","categories": [],
         "tags": [],
-        "url": "/updates/new-update/"
+        "url": "https://www.chrisfiola.ca/home/updates/new-update/"
       },]
