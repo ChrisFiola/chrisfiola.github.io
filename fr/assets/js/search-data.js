edd5c99da0c1a8dd547a7d517fa85cde37801fce
ChrisFiola <36894544+ChrisFiola@users.noreply.github.com>
2022-05-06 04:00:51 +0000

    Deploying to gh-pages from @ ChrisFiola/chrisfiola.github.io@7415148d4d420f07cf7cf0941df8aaf85d2daa66 🚀

---
+++ fr/assets/js/search-data.js
@@ -0,0 +1,21 @@
+var store = [{
+        "title": "hof.hof2122.title",
+        "excerpt":"(fr test) You will find the best results of the season on this page. Best 500m of the season 🥇 Best 1000m of the season 🥇 Best 1500m of the season 🥇 Season Bests Season Bests Christopher Fiola (CAN) 500 34,44 Calgary (CAN) 2022-03-19 1000 1.08,43 Calgary (CAN) 2022-03-19 1500...","categories": [],
+        "tags": [],
+        "url": "/hof/hof2122/"
+      },{
+        "title": "updates.new-season.title",
+        "excerpt":"(fr test) After a month off I am finally back in training for the 2022-2023 season! Welcome Back! Coming off a strong end of the season in 2021-2022, I couldn’t wait to go back to training and push myself again. Last season was very odd and I had to overcome...","categories": [],
+        "tags": [],
+        "url": "/updates/new-season/"
+      },{
+        "title": "updates.new-update.title",
+        "excerpt":"(fr test)So I have decided to create a website about myself. Hopefully this will help promote me and eventually gather more attention into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general...","categories": [],
+        "tags": [],
+        "url": "/updates/new-update/"
+      },{
+        "title": "wip.new-website.title",
+        "excerpt":"(fr test) Here you will find the development process of this website and what I am trying to make out of it. Why a website ? I have decided to create a website for myself as a way to promote what I am doing in sports and gather more attention...","categories": [],
+        "tags": [],
+        "url": "/wip/new-website/"
+      }]
---
+++ fr/docs/README.md
@@ -0,0 +1,21 @@
+Personal website (work in progess)
+
+Based on : https://github.com/mmistakes/so-simple-theme
+
+Master Branch:
+- Contains all the necessary files to build the site with jekyll-so-simple-theme.
+- Add posts in the `_updates` folder.
+- Update the homepage in the `home.md` file.
+
+`/docs`:
+- Contains the build which is built locally using `bundle exec jekyll build --trace`
+- The destination of the build is set in the _config.yml
+
+To build the site:
+- Delete `vendor` on the master branch which contains ruby
+- On terminal locally (macos) type:
+   - `chrisfiola.github.io % bundle config set --local path vendor/bundle`
+   - `chrisfiola.github.io % bundle install`
+   - `chrisfiola.github.io % bundle exec jekyll build --trace`
+
+Calendar Integration using tockify.com
