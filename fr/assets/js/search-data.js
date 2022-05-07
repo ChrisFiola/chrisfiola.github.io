73d9bcac09d8ea3e26a96693056a79f8f9910ab3
ChrisFiola <36894544+ChrisFiola@users.noreply.github.com>
2022-05-07 01:38:19 +0000

    Deploying to gh-pages from @ ChrisFiola/chrisfiola.github.io@2df62ddfc43c802bc991e91467645910d600203c 🚀

--- fr/assets/js/search-data.js
+++ fr/assets/js/search-data.js
@@ -1,21 +1,41 @@
 var store = [{
-        "title": "hof.hof2122.title",
+        "title": "2021-2022",
+        "excerpt":"You will find the best results of the season on this page. Best 500m of the season 🥇 Best 1000m of the season 🥇 Best 1500m of the season 🥇 Season Bests Season Bests Christopher Fiola (CAN) 500 34,44 Calgary (CAN) 2022-03-19 1000 1.08,43 Calgary (CAN) 2022-03-19 1500 1.46,86 Calgary...","categories": [],
+        "tags": [],
+        "url": "/hof/en/hof2122/"
+      },{
+        "title": "2021-2022",
         "excerpt":"(fr test) You will find the best results of the season on this page. Best 500m of the season 🥇 Best 1000m of the season 🥇 Best 1500m of the season 🥇 Season Bests Season Bests Christopher Fiola (CAN) 500 34,44 Calgary (CAN) 2022-03-19 1000 1.08,43 Calgary (CAN) 2022-03-19 1500...","categories": [],
         "tags": [],
-        "url": "/hof/hof2122/"
+        "url": "/hof/fr/hof2122/"
       },{
-        "title": "updates.new-season.title",
-        "excerpt":"(fr test) After a month off I am finally back in training for the 2022-2023 season! Welcome Back! Coming off a strong end of the season in 2021-2022, I couldn’t wait to go back to training and push myself again. Last season was very odd and I had to overcome...","categories": [],
+        "title": "Start of the 2022-2023 season!",
+        "excerpt":"(en) After a month off I am finally back in training for the 2022-2023 season! Welcome Back! Coming off a strong end of the season in 2021-2022, I couldn’t wait to go back to training and push myself again. Last season was very odd and I had to overcome a...","categories": [],
         "tags": [],
-        "url": "/updates/new-season/"
+        "url": "/updates/new-season"
       },{
-        "title": "updates.new-update.title",
+        "title": "Welcome to my first post and to my new website",
+        "excerpt":"So I have decided to create a website about myself. Hopefully this will help promote me and eventually gather more attention into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general information...","categories": [],
+        "tags": [],
+        "url": "/updates/new-update"
+      },{
+        "title": "Debut de la saison 2022-2023!",
+        "excerpt":"(fr) After a month off I am finally back in training for the 2022-2023 season! Welcome Back! Coming off a strong end of the season in 2021-2022, I couldn’t wait to go back to training and push myself again. Last season was very odd and I had to overcome a...","categories": [],
+        "tags": [],
+        "url": "/fr/updates/new-season"
+      },{
+        "title": "Bienvenue a ma premiere publication et a mon nouveau site web",
         "excerpt":"(fr test)So I have decided to create a website about myself. Hopefully this will help promote me and eventually gather more attention into what I’m working towards. The ultimate goal is Milan Cortina 2026 and we will see after… Site’s Content 1. Homepage: This is where you will find general...","categories": [],
         "tags": [],
-        "url": "/updates/new-update/"
+        "url": "/fr/updates/new-update"
       },{
-        "title": "wip.new-website.title",
-        "excerpt":"(fr test) Here you will find the development process of this website and what I am trying to make out of it. Why a website ? I have decided to create a website for myself as a way to promote what I am doing in sports and gather more attention...","categories": [],
+        "title": "Website under construction 🚧",
+        "excerpt":"Here you will find the development process of this website and what I am trying to make out of it. Why a website ? I have decided to create a website for myself as a way to promote what I am doing in sports and gather more attention towards my...","categories": [],
         "tags": [],
-        "url": "/wip/new-website/"
+        "url": "/wip/en/new-website/"
+      },{
+        "title": "Site en construction 🚧",
+        "excerpt":"Here you will find the development process of this website and what I am trying to make out of it. Why a website ? I have decided to create a website for myself as a way to promote what I am doing in sports and gather more attention towards my...","categories": [],
+        "tags": [],
+        "url": "/wip/fr/new-website/"
       }]
